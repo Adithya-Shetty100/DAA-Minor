2d9b5a64146d6e0b1949ab95bbe02b0a98db1352
Adithya B Shetty <adithyabshetty100@gmail.com>
2021-04-30 22:16:07 +0530

    Delete week4 directory

--- week4/mergeSort.h
+++
@@ -1,7 +0,0 @@
-// Merges two sorted halfs of an array into a single sorted array
-void mergeSortedHalfs(int *a, int start, int mid, int end);
-
-// Implement merge sort using the mergeSortedHalfs() declared above
-// start is the index of the first element of the array
-// end is the index of the last element of the array
-void mergeSort(int *a, int start, int end);
